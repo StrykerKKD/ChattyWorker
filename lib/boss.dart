2df358d776d415414062d41edd4751ab45ef3bc9
StrykerKKD <kkdstryker@gmail.com>
2015-01-01 18:26:00 +0100

    making a simpler spawning isolate

--- lib/boss.dart
+++ lib/boss.dart
@@ -1,35 +1,31 @@
 part of ChattyWorker;
 
-_startWorker(msg) {
-  print(Boss.workers);
-  Boss.workers[msg["worker"]].startWorking(msg["sendPort"]);
+_manager(message) {
+  message[0].startWorking(message[1]);
 }
+
 
 abstract class Boss {
 
   ReceivePort response;
   SendPort request;
-  static Map<String,Worker> workers;
-  Map<String,Isolate> isoWorkers;
-
+  
   Boss() {
     response = new ReceivePort();
-    workers = new Map<String,Worker>();
-    isoWorkers = new Map<String, Isolate>();
     response.listen((message) {
       if(message is SendPort) {
+        print('Boss:A');
         request = message;
+        speak("ready");
       } else {
+        print('Boss:B');
         manage(message);
       }
     });
   }
 
   void doYourJob(Worker worker) {
-    workers[worker.name] = worker;
-    Isolate.spawn(_startWorker, {"sendPort":response.sendPort,"worker":worker.name}).then((isoWorker) {
-      isoWorkers[worker.name] = isoWorker;
-    });
+    Isolate.spawn(_manager, [worker, response.sendPort]);
   }
 
   void manage(message);
